9eb778551851a2752ba1b0da9afe44f106b16770
Yuhan Zhang <121049397+ZzhangYH@users.noreply.github.com>
2025-07-26 17:05:55 +0800

    feat: build program docs

--- feiyue/docs.py
+++ feiyue/docs.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
@@ -9,6 +10,7 @@ def build_pages(records: list[dict], template: str, resources: str, output: str)
     mkdocs = MkDocs(template, resources, output)
     mkdocs.pre_build(records)
     mkdocs.build_applicants()
+    mkdocs.build_programs()
 
 
 class MkDocs:
@@ -44,7 +46,7 @@ class MkDocs:
                 "Admit": ":green_circle: Admit",
                 "Reject": ":red_circle: Reject",
                 "Waitlist": ":yellow_circle: Waitlist",
-                "Chosen": ":white_check_mark: Chosen",
+                "Chosen": ":checkered_flag: Chosen",
             }
         })
 
@@ -52,15 +54,41 @@ class MkDocs:
         applicants_path = self.output_path / "applicants"
         applicants_path.mkdir(exist_ok=True)
 
+        # Individual applicant pages
+        for student in self.students.values():
+            template = self.env.get_template("applicant.jinja")
+            output = template.render(
+                student=student
+            )
+            with open(applicants_path / f"{student['s_id']}.md", "w") as f:
+                f.write(output)
 
-
-        temp_index = self.env.get_template("applicant_index.jinja")
-        output = temp_index.render(students=list(self.students.values()))
+        # Applicants index page
+        template_index = self.env.get_template("applicant_index.jinja")
+        output = template_index.render(students=list(self.students.values()))
         with open(applicants_path / "index_applicants.md", "w") as f:
             f.write(output)
 
-        for _, student in self.students.items():
-            temp_applicant = self.env.get_template("applicant.jinja")
-            output = temp_applicant.render(student=student)
-            with open(applicants_path / f"{student['s_id']}.md", "w") as f:
+    def build_programs(self):
+        programs_path = self.output_path / "programs"
+        programs_path.mkdir(exist_ok=True)
+
+        # Individual program pages
+        for program in self.programs.values():
+            # Group program applications by student terms
+            applications_by_term = defaultdict(list)
+            for application in self.applications.values():
+                if application["program"][0]["row_id"] == program["_id"]:
+                    student = self.students[application["student"][0]["row_id"]]
+                    applications_by_term[student["term"]].append(application)
+
+            template = self.env.get_template("program.jinja")
+            output = template.render(
+                program=program,
+                applications_by_term=applications_by_term
+            )
+            with open(programs_path / f"{program['p_id']}.md", "w") as f:
                 f.write(output)
+
+        # Programs index page
+        # TODO
